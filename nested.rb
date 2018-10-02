9d5e52bb524c12514f76168caf858194c13cae4a
Brian Birdwell <bbirdwell@gmail.com>
2018-10-02 16:18:00 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages].join()
+     
+    
 end
 
 def adding_matz
@@ -80,7 +84,9 @@ def adding_matz
         }
      }
 
-    
+    programmer_hash[:yukihiro_matsumoto] = "matzo"
+    # programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+    # programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
 end
 
 def changing_alan
